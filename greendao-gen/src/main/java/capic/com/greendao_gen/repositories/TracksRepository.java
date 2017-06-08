224f824db6598d02eb72494eae3d2f2025224dde
capic <vcapic@gmail.com>
2017-06-08 20:55:01 +0200

    have to check il all is ok

---
+++ greendao-gen/src/main/java/capic/com/greendao_gen/repositories/TracksRepository.java
@@ -0,0 +1,10 @@
+package capic.com.greendao_gen.repositories;
+
+import capic.com.karttracker.services.datas.repositories.TracksRespository;
+
+/**
+ * Created by capic on 02/05/2017.
+ */
+
+public class TracksRepository implements TracksRespository {
+}
